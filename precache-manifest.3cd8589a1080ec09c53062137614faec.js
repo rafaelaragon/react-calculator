161df897f35d99e00e29cd127fa0ca005ad41f94
Aragon Rodriguez <r.aragon.rodriguez@accenture.com>
2020-01-13 09:00:17 +0100

    Updates

---
+++ precache-manifest.3cd8589a1080ec09c53062137614faec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cd5a74cea6d50b73641fa09d72fecd",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "4f333690f21f6458a38a",
+    "url": "/react-calculator/static/css/main.218aea77.chunk.css"
+  },
+  {
+    "revision": "bbff726818dc8935988a",
+    "url": "/react-calculator/static/js/2.7a40189c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calculator/static/js/2.7a40189c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f333690f21f6458a38a",
+    "url": "/react-calculator/static/js/main.1a97a4a8.chunk.js"
+  },
+  {
+    "revision": "76f1eb14f403acfb6a7d",
+    "url": "/react-calculator/static/js/runtime-main.6093da4d.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/react-calculator/static/media/digital-7.regular.1e670d88.ttf"
+  }
+]);
